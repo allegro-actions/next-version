77ea74078224a8a7739d074249f2927cee9104f2
Bartosz Galek <bartosz.galek@allegro.pl>
2021-08-19 23:45:14 +0200

    dependencies updated, gh-stats reporter added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const core = require('@actions/core');
+const { reportAction } = require('@gh-stats/reporter');
 const action = require('./action');
-
 const prefix = core.getInput('prefix');
 const versioning = core.getInput('versioning');
 const force = core.getInput('force');
+
+reportAction();
 
 const { currentTag, nextTag, nextVersion } = action({ prefix, versioning, force });
 core.setOutput('current_tag', currentTag);
