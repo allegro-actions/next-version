faaecc7812c9710e9c1b3024770e2a1d9591e7b5
Tomasz Jackowiak <tjackowiak@users.noreply.github.com>
2021-09-03 16:46:31 +0200

    Update git-commands.js

--- git-commands.js
+++ git-commands.js
@@ -5,7 +5,7 @@ const TAG_NOT_FOUND = 128;
 module.exports = {
   getLatestTag: (prefix) => {
     try {
-      const stdout = exec(`git describe --match "${prefix}*" --abbrev=0 --tags`);
+      const stdout = exec(`git tag -l | grep "${prefix}*" | sort -V | tail -n 1`);
       return String(stdout).trim();
     } catch (error) {
       if (error.status === TAG_NOT_FOUND) return null;
