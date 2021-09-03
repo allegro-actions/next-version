9a706257d036053f02ed023e132d9cddd6d83208
Wioleta Gozdzik <wioleta.gozdzik@allegro.pl>
2021-09-03 13:08:24 +0200

    OPBOX-8941_opbox-web-release

--- action.spec.js
+++ action.spec.js
@@ -4,16 +4,83 @@ describe('action', () => {
 
   test('increments version', () => {
     // expect
-    expect(action({ versioning: 'semver' }, () => 'v1.0.0')).toEqual({
-      'currentTag': 'v1.0.0',
-      'nextTag': 'v1.0.1',
-      'nextVersion': '1.0.1'
+    expect(action({ versioning: 'semver' }, () => 'v1.0.1')).toEqual({
+      'currentTag': 'v1.0.1',
+      'nextTag': 'v1.0.2',
+      'nextVersion': '1.0.2'
     });
     expect(action({ versioning: 'single-number' }, () => 'v1')).toEqual({
       'currentTag': 'v1',
       'nextTag': 'v2',
       'nextVersion': '2'
     });
+  });
+
+  test('increments pre release version', () => {
+    // expect
+    expect(action({ versioning: 'semver', preReleaseSuffix: 'RC', level: 'prerelease' }, () => 'v1.0.0')).toEqual({
+      'currentTag': 'v1.0.0',
+      'nextTag': 'v1.0.1-RC.0',
+      'nextVersion': '1.0.1-RC.0'
+    });
+    expect(action({ versioning: 'single-number', preReleaseSuffix: 'RC', level: 'prerelease' }, () => 'v1')).toEqual({
+      'currentTag': 'v1',
+      'nextTag': 'v2',
+      'nextVersion': '2'
+    });
+    expect(action({ versioning: 'semver', preReleaseSuffix: 'RC', level: 'prerelease' }, () => 'v1.0.0-RC.0')).toEqual({
+      'currentTag': 'v1.0.0-RC.0',
+      'nextTag': 'v1.0.0-RC.1',
+      'nextVersion': '1.0.0-RC.1'
+    });
+    expect(action({ versioning: 'single-number', preReleaseSuffix: 'RC', level: 'prerelease' }, () => 'v1-RC.0')).toEqual({
+      'currentTag': 'v1-RC.0',
+      'nextTag': 'v2',
+      'nextVersion': '2'
+    });
+  });
+
+  test('remove pre release suffix for patch level', () => {
+    // expect
+    expect(action({ versioning: 'semver', preReleaseSuffix: 'RC', level: 'patch' }, () => 'v1.0.0-RC.0')).toEqual({
+      'currentTag': 'v1.0.0-RC.0',
+      'nextTag': 'v1.0.0',
+      'nextVersion': '1.0.0'
+    });
+    expect(action({ versioning: 'single-number', preReleaseSuffix: 'RC', level: 'patch' }, () => 'v1-RC.0')).toEqual({
+      'currentTag': 'v1-RC.0',
+      'nextTag': 'v2',
+      'nextVersion': '2'
+    });
+  });
+
+
+
+
+  test('when there is no suffix for pre release', () => {
+    // expect
+    expect(() => action({  versioning: 'semver', preReleaseSuffix: '', level: 'prerelease'  }, () => 'v1.0.0-RC.0')).toThrowError();
+    expect(() => action({  versioning: 'single-number', preReleaseSuffix: '', level: 'prerelease'  }, () => 'v1-RC.0')).toThrowError();
+  });
+
+  test('sets initial pre-release version when no release tag found', () => {
+    // expect
+    expect(action({ versioning: 'semver', preReleaseSuffix: 'RC', level: 'prerelease' }, () => null)).toEqual({
+      'currentTag': '',
+      'nextTag': 'v0.0.1-RC.0',
+      'nextVersion': '0.0.1-RC.0'
+    });
+    expect(action({ versioning: 'single-number', preReleaseSuffix: 'RC', level: 'prerelease' }, () => null)).toEqual({
+      'currentTag': '',
+      'nextTag': 'v1-RC.0',
+      'nextVersion': '1-RC.0'
+    });
+  });
+
+  test('throws error when level is invalid', () => {
+    // expect
+    expect(() => action({ versioning: 'semver', preReleaseSuffix: 'RC', level: 'invalid' }, () =>  'v1.0.0-RC.0')).toThrowError();
+    expect(() => action({ versioning: 'single-number', preReleaseSuffix: 'RC', level: 'invalid' }, () =>  'v1-RC.0')).toThrowError();
   });
 
   test('increments version with custom prefix', () => {
