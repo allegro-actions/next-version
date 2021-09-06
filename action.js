009d5cda72557245b6abf1a9a82f6d6524baf0a7
wgozdzik <wioleta.gozdzik@gmail.com>
2021-09-06 09:44:17 +0200

    pre-release-version-bump (#13)
    * pre-release-version-bupm
    
    * final-v1
    
    * some-clean-up-code

--- action.js
+++ action.js
@@ -8,9 +8,10 @@ const { getLatestTag } = require('./git-commands');
  * @param tagExtractor
  * @returns {string|*}
  */
-module.exports = function action({ prefix = 'v', versioning = 'semver', force }, tagExtractor = getLatestTag) {
+module.exports = function action({ prefix = 'v', versioning = 'semver', force, preReleaseSuffix = '', level = 'patch' }, tagExtractor = getLatestTag) {
 
   const latestTag = tagExtractor(prefix);
+  const PRERELEASE_LEVEL_NAME = 'prerelease';
 
   if (force) return { 'currentTag': latestTag || '', 'nextTag': prefix + force, 'nextVersion': force };
 
@@ -18,16 +19,36 @@ module.exports = function action({ prefix = 'v', versioning = 'semver', force },
     throw new Error(`unknown versioning '${versioning}'`);
   }
 
-  if (latestTag === null) {
-    switch (versioning) {
-      case 'semver': {
-        return { currentTag: '', nextTag: `${prefix}0.0.1`, nextVersion: '0.0.1' };
-      }
-      case 'single-number': {
-        return { currentTag: '', nextTag: `${prefix}1`, nextVersion: '1' };
-      }
-    }
+  if (!['major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 'prerelease'].includes(level)) {
+    throw new Error(`Invalid level name: ${level}`);
   }
+
+  function isPreReleaseLevel() {
+    return level === PRERELEASE_LEVEL_NAME;
+  }
+
+  if (isPreReleaseLevel() && preReleaseSuffix === '') {
+    throw new Error(`There is no pre release suffix for level: ${level}`);
+  }
+
+  if (latestTag === null) {
+    let calculatedPreReleaseVersion = '';
+
+    if (level === PRERELEASE_LEVEL_NAME) {
+      calculatedPreReleaseVersion = `-${preReleaseSuffix}.0`;
+    }
+    let calculatedNextVersion = '';
+
+    if ( versioning === 'semver') calculatedNextVersion = `0.0.1${calculatedPreReleaseVersion}`;
+    if ( versioning === 'single-number') calculatedNextVersion = `1${calculatedPreReleaseVersion}`;
+
+    return {
+      currentTag: '',
+      nextTag: `${prefix}${calculatedNextVersion}`,
+      nextVersion: `${calculatedNextVersion}`
+    };
+  }
+
   if (!latestTag.startsWith(prefix)) {
     throw new Error(`expecting provided tag ${latestTag} to start with ${prefix}`);
   }
@@ -36,16 +57,41 @@ module.exports = function action({ prefix = 'v', versioning = 'semver', force },
   switch (versioning) {
     case 'semver': {
       if (!semver.valid(version)) throw new Error(`version ${version} not a valid semver string`);
-      return {
-        currentTag: latestTag,
-        nextTag: `${prefix}${semver.inc(version, 'patch')}`,
-        nextVersion: semver.inc(version, 'patch')
-      };
+        return {
+          currentTag: latestTag,
+          nextTag: `${prefix}${semver.inc(version, level, preReleaseSuffix)}`,
+          nextVersion: semver.inc(version, level, preReleaseSuffix)
+        };
     }
     case 'single-number': {
-      const versionInt = parseInt(version, 10);
-      if (isNaN(versionInt)) throw new Error(`version ${version} not a valid number`);
-      return { currentTag: latestTag, nextTag: `${prefix}${versionInt + 1}`, nextVersion: `${versionInt + 1}` };
+      let calculatedNextVersion = '';
+      const isPreReleasedTag = version.includes('-' + preReleaseSuffix);
+
+      const [singleNumberVersion, suffix] = version.split(`-${preReleaseSuffix}.`, 2);
+      const nextPreReleaseVersion = suffix ? parseInt(suffix,10) + 1 : 0;
+
+      if (isPreReleasedTag && isPreReleaseLevel()) {
+        calculatedNextVersion = `${singleNumberVersion}-${preReleaseSuffix}.${nextPreReleaseVersion}`;
+      }
+
+      if (isPreReleasedTag && !isPreReleaseLevel()) {
+        calculatedNextVersion = parseInt(version, 10);
+        if (isNaN(calculatedNextVersion)) throw new Error(`version ${version} not a valid number`);
+      }
+
+      if (!isPreReleasedTag && isPreReleaseLevel()) {
+        calculatedNextVersion = `${parseInt(singleNumberVersion, 10) + 1}-${preReleaseSuffix}.${nextPreReleaseVersion}`;
+      }
+
+      if (!isPreReleasedTag && !isPreReleaseLevel()) {
+        calculatedNextVersion = `${parseInt(singleNumberVersion, 10) + 1}`;
+      }
+
+      return {
+        currentTag: latestTag,
+        nextTag: `${prefix}${calculatedNextVersion}`,
+        nextVersion: `${calculatedNextVersion}`
+      };
     }
   }
 };
--- action.spec.js
+++ action.spec.js
@@ -16,6 +16,70 @@ describe('action', () => {
     });
   });
 
+  test('increments pre release version', () => {
+    // expect
+    expect(action({ versioning: 'semver', preReleaseSuffix: 'RC', level: 'prerelease' }, () => 'v1.0.0')).toEqual({
+      'currentTag': 'v1.0.0',
+      'nextTag': 'v1.0.1-RC.0',
+      'nextVersion': '1.0.1-RC.0'
+    });
+    expect(action({ versioning: 'single-number', preReleaseSuffix: 'RC', level: 'prerelease' }, () => 'v1')).toEqual({
+      'currentTag': 'v1',
+      'nextTag': 'v2-RC.0',
+      'nextVersion': '2-RC.0'
+    });
+    expect(action({ versioning: 'semver', preReleaseSuffix: 'RC', level: 'prerelease' }, () => 'v1.0.0-RC.0')).toEqual({
+      'currentTag': 'v1.0.0-RC.0',
+      'nextTag': 'v1.0.0-RC.1',
+      'nextVersion': '1.0.0-RC.1'
+    });
+    expect(action({ versioning: 'single-number', preReleaseSuffix: 'RC', level: 'prerelease' }, () => 'v1-RC.1')).toEqual({
+      'currentTag': 'v1-RC.1',
+      'nextTag': 'v1-RC.2',
+      'nextVersion': '1-RC.2'
+    });
+  });
+
+  test('remove pre release suffix for patch level', () => {
+    // expect
+    expect(action({ versioning: 'semver', preReleaseSuffix: 'RC', level: 'patch' }, () => 'v1.0.0-RC.1')).toEqual({
+      'currentTag': 'v1.0.0-RC.1',
+      'nextTag': 'v1.0.0',
+      'nextVersion': '1.0.0'
+    });
+    expect(action({ versioning: 'single-number', preReleaseSuffix: 'RC', level: 'patch' }, () => 'v5-RC.1')).toEqual({
+      'currentTag': 'v5-RC.1',
+      'nextTag': 'v5',
+      'nextVersion': '5'
+    });
+  });
+
+  test('when there is no suffix for pre release', () => {
+    // expect
+    expect(() => action({  versioning: 'semver', preReleaseSuffix: '', level: 'prerelease'  }, () => 'v1.0.0-RC.0')).toThrowError();
+    expect(() => action({  versioning: 'single-number', preReleaseSuffix: '', level: 'prerelease'  }, () => 'v1-RC.0')).toThrowError();
+  });
+
+  test('sets initial pre-release version when no release tag found', () => {
+    // expect
+    expect(action({ versioning: 'semver', preReleaseSuffix: 'RC', level: 'prerelease' }, () => null)).toEqual({
+      'currentTag': '',
+      'nextTag': 'v0.0.1-RC.0',
+      'nextVersion': '0.0.1-RC.0'
+    });
+    expect(action({ versioning: 'single-number', preReleaseSuffix: 'RC', level: 'prerelease' }, () => null)).toEqual({
+      'currentTag': '',
+      'nextTag': 'v1-RC.0',
+      'nextVersion': '1-RC.0'
+    });
+  });
+
+  test('throws error when level is invalid', () => {
+    // expect
+    expect(() => action({ versioning: 'semver', preReleaseSuffix: 'RC', level: 'invalid' }, () =>  'v1.0.0-RC.0')).toThrowError();
+    expect(() => action({ versioning: 'single-number', preReleaseSuffix: 'RC', level: 'invalid' }, () =>  'v1-RC.0')).toThrowError();
+  });
+
   test('increments version with custom prefix', () => {
     // expect
     expect(action({
