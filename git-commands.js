76fa9d6315de0162bb2f54854c8e19e011380525
Mariusz Wojakowski <mariusz.wojakowski@allegro.pl>
2021-09-08 08:21:32 +0200

    Fix retrieving last git tag for empty prefixes (#16)

--- git-commands.js
+++ git-commands.js
@@ -5,7 +5,8 @@ const TAG_NOT_FOUND = 128;
 module.exports = {
   getLatestTag: (prefix) => {
     try {
-      const stdout = exec(`git tag -l | grep "${prefix}*" | sort -V | tail -n 1`);
+      const maybeGrepCommand = prefix !== '' ? `| grep "${prefix}*"` : '';
+      const stdout = exec(`git tag -l ${maybeGrepCommand} | sort -V | tail -n 1`);
       return String(stdout).trim();
     } catch (error) {
       if (error.status === TAG_NOT_FOUND) return null;
