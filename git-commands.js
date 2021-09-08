7af8e2750885ce3b80d0dc8e59bcd008869507cc
wszostak <wszostak@users.noreply.github.com>
2021-09-08 12:32:17 +0200

    Change grep pattern
    When `pattern` is one character long then `grep "${prefix}*"` returns all results. This is because `<char>*` means "zero or more <char>".

--- git-commands.js
+++ git-commands.js
@@ -5,7 +5,7 @@ const TAG_NOT_FOUND = 128;
 module.exports = {
   getLatestTag: (prefix) => {
     try {
-      const maybeGrepCommand = prefix !== '' ? `| grep "${prefix}*"` : '';
+      const maybeGrepCommand = prefix !== '' ? `| grep "^${prefix}"` : '';
       const stdout = exec(`git tag -l ${maybeGrepCommand} | sort -V | tail -n 1`);
       return String(stdout).trim();
     } catch (error) {
