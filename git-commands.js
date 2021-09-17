3cfc6945563e383cecb7c9d91688f516edc4c063
Tomasz Jackowiak <tomasz.jackowiak@allegro.pl>
2021-09-17 10:40:09 +0200

    OPBOX-8951 | Fixed setting initial value when no tags were found

--- git-commands.js
+++ git-commands.js
@@ -1,16 +1,13 @@
-const { execSync: exec } = require('child_process');
-
-const TAG_NOT_FOUND = 128;
+const {execSync: exec} = require('child_process');
 
 module.exports = {
+  /*
+   * Returns latest tag, sorted by version or null if no tag was found
+   */
   getLatestTag: (prefix) => {
-    try {
-      const maybeGrepCommand = prefix !== '' ? `| grep "${prefix}*"` : '';
-      const stdout = exec(`git tag -l ${maybeGrepCommand} | sort -V | tail -n 1`);
-      return String(stdout).trim();
-    } catch (error) {
-      if (error.status === TAG_NOT_FOUND) return null;
-      throw error;
-    }
+    const maybeGrepCommand = prefix !== '' ? `| grep "${prefix}*"` : '';
+    const stdout = exec(`git tag -l ${maybeGrepCommand} | sort -V | tail -n 1`);
+    const lastTag = String(stdout).trim();
+    return lastTag === '' ? null : lastTag;
   }
 };
